aa4d4efa7023f51504c49df6f1ccbec2eab9da22
Akshay Aravind <aa2288@cornell.edu>
2023-11-03 20:10:37 -0400

    Completed questions 1-5 for assignment 1

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 // QUESTION 1 ------------------------------------------------------------------
 
 export const arrayMean = (array: number[]): number | undefined => {
-    // TODO: Implement!
-    return 0;
+    let sum: number = 0;
+    for (const val of array) {
+        sum += val;
+    }
+    return sum / array.length;
 };
 
 // QUESTION 2 ------------------------------------------------------------------
@@ -16,28 +19,52 @@ export const affordableHousing = (
     listings: Apartment[],
     budget: number
 ): string[] => {
-    // TODO: Implement!
-    return ["Hello", "World"];
+    let res: string[] = []
+    for (const listing of listings) {
+        if (listing.rent <= budget) {
+            res.push(listing.id)
+        }
+    }
+    return res;
 };
 
 // QUESTION 3 ------------------------------------------------------------------
 
 export const evenEven = (array?: string[]): boolean => {
-    // TODO: Implement!
-    return false;
+    if (array === undefined) {
+        return true;
+    }
+
+    for (const str of array) {
+        if (str.length % 2 === 0 && !str.includes('even')) {
+            return false;
+        }
+    }
+
+    return true;
 };
+
 
 // QUESTION 4 ------------------------------------------------------------------
 
 // TODO: You should replace this "any" with an accurate type in your submission!
-export type Doggo = any;
+export type Doggo = { name: string, age: number, breed: string };
 
 export const makeSentences = (array: Doggo[]): string[] => {
-    // TODO: Implement!
-    return ["Hello", "World"];
+    let res: string[] = []
+    for (const a of array) {
+        let curr: string = a.name + " is " + Math.round(a.age * 10) / 10 + " years old, and is a " + a.breed + ".";
+        res.push(curr);
+    }
+    return res;
 };
 
 // DEMO ------------------------------------------------------------------------
+const doggos = [
+    { name: 'Sparky', age: 3.35, breed: 'Pomeranian Husky' },
+    { name: 'Oreo', age: 5.42, breed: 'Dalmatian' },
+    { name: 'Stella', age: 4, breed: 'Alaskan Klee Kai' },
+];
 
-// TODO: You can remove this!
-console.log("Hello World!");
+makeSentences(doggos);
+
